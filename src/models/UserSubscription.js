0471b99794651897818ce9d59119a9aa1aabf8da
agabir <agabir@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2018-06-27 17:26:34 +0000

    added manage subscriptions model
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@2175 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/models/UserSubscription.js
+++ src/models/UserSubscription.js
@@ -1,5 +1,5 @@
 module.exports = function (app) {
-    app.factory('UserSubscription', function (CMSModelInterceptor) {
+    app.factory('UserSubscription', function (CMSModelInterceptor,langService) {
         'ngInject';
         return function UserSubscription(model) {
             var self = this;
@@ -12,7 +12,6 @@ module.exports = function (app) {
             self.status = true;
             self.ouId = null;
             self.updatedOn = null;
-            self.docClass = null;
             self.docSubject = null;
 
             // every model has required fields
@@ -21,7 +20,6 @@ module.exports = function (app) {
                 'trigerId',
                 'documentVSId',
                 'status',
-                'docClass',
                 'docSubject'
             ];
 
@@ -36,7 +34,13 @@ module.exports = function (app) {
                 return requiredFields;
             };
 
-
+            /**
+             * @description Get the status of User Subscription as Active or Inactive instead of true or false.
+             * @returns {string}
+             */
+            UserSubscription.prototype.getTranslatedStatus = function () {
+                return this.status ? langService.get('active') : langService.get('inactive');
+            };
 
             // don't remove CMSModelInterceptor from last line
             // should be always at last thing after all methods and properties.
