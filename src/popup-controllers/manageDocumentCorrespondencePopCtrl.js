bde057f34effd01c8ca378837211018917631d62
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2018-07-03 11:49:27 +0000

    fix: Issues for 2.0.2
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@2196 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/popup-controllers/manageDocumentCorrespondencePopCtrl.js
+++ src/popup-controllers/manageDocumentCorrespondencePopCtrl.js
@@ -14,6 +14,48 @@ module.exports = function (app) {
             self.correspondence.sitesInfoTo = sites.first;
             self.correspondence.sitesInfoCC = sites.second;
         }
+
+        self.needReply = function (status) {
+            return (status && status.lookupStrKey === 'NEED_REPLY');
+        };
+
+        /**
+         * @description Checks if added correspondence sites are valid.
+         * If followup status = Need reply, it should have followup date too.
+         * In case of outgoing, at least one original site should added.
+         * @returns {boolean}
+         * If true, the correspondence sites is invalid and disable the save button
+         */
+        self.checkDisabled = function () {
+            var i, record;
+            if (self.documentClass.toLowerCase() === 'outgoing') {
+                if (self.correspondence.hasSiteTO()) {
+                    var inValidSitesCount = 0;
+                    for (i = 0; i < self.correspondence.sitesInfoTo.length; i++) {
+                        record = self.correspondence.sitesInfoTo[i];
+                        if (self.needReply(record.followupStatus) && !record.followupDate) {
+                            inValidSitesCount++;
+                        }
+                    }
+                    for (i = 0; i < self.correspondence.sitesInfoCC.length; i++) {
+                        record = self.correspondence.sitesInfoCC[i];
+                        if (self.needReply(record.followupStatus) && !record.followupDate) {
+                            inValidSitesCount++;
+                        }
+                    }
+                    return !!inValidSitesCount;
+                }
+                return true;
+            }
+            else {
+                if (self.correspondence.site) {
+                    record = self.correspondence.site;
+                    return !!(self.needReply(record.followupStatus) && !record.followupDate);
+                }
+                return true;
+            }
+        };
+
         self.saveCorrespondenceSites = function () {
             self.correspondence
                 .updateSites()
