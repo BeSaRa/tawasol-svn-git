a9119a4a5ff179e0e521cb37be20a50228528181
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2018-07-05 15:24:31 +0000

    add report menu items
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@2220 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/services/reportService.js
+++ src/services/reportService.js
@@ -9,6 +9,51 @@ module.exports = function (app) {
                 id: 1,
                 reportName: 'تقرير احصاء المدخلات',
                 langKey: 'menu_item_reports_statistical_report'
+            }),
+            new Report({
+                id: 2,
+                reportName: 'تقرير احصاء المراسلات',
+                langKey: 'menu_item_reports_statistical_correspondence_report'
+            }),
+            new Report({
+                id: 3,
+                reportName: 'تقرير الدورات المستندية',
+                langKey: 'menu_item_reports_documentary_report'
+            }),
+            new Report({
+                id: 4,
+                reportName: 'تقرير المتابعه',
+                langKey: 'menu_item_reports_followup_report'
+            }),
+            new Report({
+                id: 5,
+                reportName: 'تقرير بريد المستخدم',
+                langKey: 'menu_item_reports_user_mail_report'
+            }),
+            new Report({
+                id: 6,
+                reportName: 'سجل عمليات الدخول للنظام',
+                langKey: 'menu_item_reports_login_logs_report'
+            }),
+            new Report({
+                id: 7,
+                reportName: 'لوحة استخدام النظام',
+                langKey: 'menu_item_reports_system_usage_report'
+            }),
+            new Report({
+                id: 8,
+                reportName: 'لوحة بيان المراسلات التي بحاجة الى رد',
+                langKey: 'menu_item_reports_message_board_response'
+            }),
+            new Report({
+                id: 9,
+                reportName: 'لوحة تقييم أداء مستخدم',
+                langKey: 'menu_item_reports_user_performance_panel'
+            }),
+            new Report({
+                id: 10,
+                reportName: 'لوحة رصد الدورات المستندية للمراسلات',
+                langKey: 'menu_item_reports_monitoring_correspondence_documentary_panel'
             })
         ];
         /**
