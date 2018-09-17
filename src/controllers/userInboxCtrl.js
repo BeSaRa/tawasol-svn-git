0dd62d5a743ccc810815b265e08a8bf2ea73b0ca
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2018-09-17 08:02:22 +0000

    change: added permission for edit in desktop
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@2487 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/controllers/userInboxCtrl.js
+++ src/controllers/userInboxCtrl.js
@@ -1511,14 +1511,24 @@ module.exports = function (app) {
                 shortcut: true,
                 hide: false,
                 callback: self.editInDesktop,
-                class: "action-red",
+                class: "action-green",
                 showInView: false,
                 checkShow: function (action, model) {
                     var info = model.getInfo();
+                    var hasPermission = false;
+                    if(info.documentClass === 'outgoing'){
+                        hasPermission = employeeService.hasPermissionTo("EDIT_OUTGOING_CONTENT");
+                    } else if(info.documentClass === 'incoming'){
+                        hasPermission = employeeService.hasPermissionTo("EDIT_INCOMING’S_CONTENT");
+                    }
+                    else if(info.documentClass === 'internal') {
+                        hasPermission = employeeService.hasPermissionTo("EDIT_INTERNAL_PROPERTIES");
+                    }
                     return self.checkToShowAction(action, model) && !model.isBroadcasted()
                         && !info.isPaper
                         && (info.documentClass !== 'incoming')
-                        && model.needApprove();
+                        && model.needApprove()
+                        && hasPermission;
                 }
             }
         ];
