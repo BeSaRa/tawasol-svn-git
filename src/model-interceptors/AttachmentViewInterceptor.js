5b5d3160c58a5a5b57a95e949f89dcff1c020684
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2018-11-12 11:03:29 +0000

    default update action status in manage attachment
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@2806 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/model-interceptors/AttachmentViewInterceptor.js
+++ src/model-interceptors/AttachmentViewInterceptor.js
@@ -11,7 +11,7 @@ module.exports = function (app) {
             model.attachmentType = model.attachmentType && model.attachmentType.hasOwnProperty('lookupKey') ? model.attachmentType.lookupKey : model.attachmentType;
             model.securityLevel = model.securityLevel && model.securityLevel.hasOwnProperty('lookupKey') ? model.securityLevel.lookupKey : model.securityLevel;
             if (typeof model.updateActionStatus !== 'undefined' && model.updateActionStatus !== null && model.updateActionStatus !== '')
-                model.updateActionStatus = model.updateActionStatus.hasOwnProperty('id') ? model.updateActionStatus.lookupKey : model.updateActionStatus;
+                model.updateActionStatus = model.updateActionStatus.hasOwnProperty('lookupKey') ? model.updateActionStatus.lookupKey : model.updateActionStatus;
             delete model.file;
             delete model.source;
             delete model.progress;
