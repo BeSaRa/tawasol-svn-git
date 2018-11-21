5bdf7f6107dcbd7a0fd94c346ce051c50fb9a683
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2018-11-21 06:34:59 +0000

    added errorCode 2034(failed delete document)
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@2860 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/factories/errorCode.js
+++ src/factories/errorCode.js
@@ -6,6 +6,7 @@ module.exports = function (app) {
             EMPTY_RESULT: 2014,
             CAN_NOT_DELETE_LOOKUP: 3014,
             WORK_ITEM_NOT_FOUND: 2043,
+            FAILED_DELETE_DOCUMENT: 2034,
             ENTITY_NOT_FOUND: 12000,
             AUTHORIZE_FAILED: 3008,
             ROOT_ENTITY_NOT_FOUND: 1001,
