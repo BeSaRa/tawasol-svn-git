3883d8ead3ad5f73c873ada39533debe50be4a97
besara <besara@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-01-07 12:11:15 +0000

    change the url to be fix or trunk.
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@3143 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- dist/js/configuration.js
+++ dist/js/configuration.js
@@ -4,15 +4,11 @@
         .config(function (urlServiceProvider) {
             'ngInject';
             urlServiceProvider
-                .setEnvironment('stage')
-                .setBaseUrl('dev', 'http://ibm-fn-http.moj.gov.qa/CMSServices/service')
+                .setEnvironment('trunk')
+                .setBaseUrl('fix', 'http://100.100.3.220:9080/CMSService/service')
+                .setBaseUrl('trunk', 'http://100.100.3.220:9080/CMSServices/service')
                 .setBaseUrl('stage', 'http://100.100.3.220:9080/CMSServices/service')
-                .setBaseUrl('demo', 'http://78.100.174.133:9080/CMSServices/service')
                 .setBaseUrl('test', 'http://eblaepm.no-ip.org:9081/CMSServices/service')
-                .setBaseUrl('training', 'http://100.100.3.142:9080/CMSServices/service')
-                .setBaseUrl('ibm', 'http://100.100.3.107:9080/CMSServices/service')
-                .setBaseUrl('moph', 'http://10.25.255.30:9080/CMSServices/service')
-                .setBaseUrl('oracle', 'http://100.100.3.197:9080/CMSServices/service');
 
             // External Systems
             urlServiceProvider
