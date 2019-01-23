b69fb0ccdb782fdabad6bc1b9f907d761786e68a
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-01-23 12:14:47 +0000

    actions grouping searchGenral
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@3253 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/controllers/searchGeneralCtrl.js
+++ src/controllers/searchGeneralCtrl.js
@@ -747,20 +747,65 @@ module.exports = function (app) {
                 class: "action-green",
                 checkShow: self.checkToShowAction
             },
-            // Preview
+            // view
             {
                 type: 'action',
                 icon: 'book-open-variant',
-                text: 'grid_action_preview_document',
+                text: 'grid_action_view',
                 shortcut: false,
-                showInView: false,
                 callback: self.previewDocument,
                 class: "action-green",
-                permissionKey: 'VIEW_DOCUMENT',
-                checkShow: function (action, model) {
-                    //If no content or no view document permission, hide the button
-                    return self.checkToShowAction(action, model) && model.hasContent();
-                }
+                showInView: false,
+                permissionKey: [
+                    'VIEW_DOCUMENT',
+                    'VIEW_DOCUMENT_VERSION'
+                ],
+                checkAnyPermission: true,
+                checkShow: self.checkToShowAction,
+                subMenu: [
+                    // Preview
+                    {
+                        type: 'action',
+                        icon: 'book-open-variant',
+                        text: 'grid_action_preview_document',
+                        shortcut: false,
+                        showInView: false,
+                        callback: self.previewDocument,
+                        class: "action-green",
+                        permissionKey: 'VIEW_DOCUMENT',
+                        checkShow: function (action, model) {
+                            //If no content or no view document permission, hide the button
+                            return self.checkToShowAction(action, model) && model.hasContent();
+                        }
+                    },
+                    // Open
+                    {
+                        type: 'action',
+                        icon: 'book-open-page-variant',
+                        text: 'grid_action_open',
+                        shortcut: false,
+                        showInView: false,
+                        callback: self.viewDocument,
+                        class: "action-green",
+                        permissionKey: 'VIEW_DOCUMENT',
+                        checkShow: function (action, model) {
+                            //If no content or no view document permission, hide the button
+                            return self.checkToShowAction(action, model) && model.hasContent();
+                        }
+                    },
+                    // show versions
+                    {
+                        type: 'action',
+                        icon: 'animation',
+                        text: 'grid_action_view_specific_version',
+                        shortcut: false,
+                        callback: self.getDocumentVersions,
+                        permissionKey: "VIEW_DOCUMENT_VERSION",
+                        class: "action-green",
+                        showInView: true,
+                        checkShow: self.checkToShowAction
+                    }
+                ]
             },
             // Separator
             {
@@ -796,21 +841,6 @@ module.exports = function (app) {
              return self.checkToShowAction(action, model) && model.docStatus < 24 && info.isPaper && info.documentClass === "outgoing";
              }
              },*/
-            // Open
-            {
-                type: 'action',
-                icon: 'book-open-page-variant',
-                text: 'grid_action_open',
-                shortcut: false,
-                showInView: false,
-                callback: self.viewDocument,
-                class: "action-green",
-                permissionKey: 'VIEW_DOCUMENT',
-                checkShow: function (action, model) {
-                    //If no content or no view document permission, hide the button
-                    return self.checkToShowAction(action, model) && model.hasContent();
-                }
-            },
             // Create Reply
             {
                 type: 'action',
@@ -1114,44 +1144,48 @@ module.exports = function (app) {
                 hide: true,
                 checkShow: self.checkToShowAction
             },
-            // show versions
+            // Duplicate
             {
                 type: 'action',
-                icon: 'animation',
-                text: 'grid_action_view_specific_version',
+                icon: 'settings',
+                text: 'grid_action_duplicate',
                 shortcut: false,
-                callback: self.getDocumentVersions,
-                permissionKey: "VIEW_DOCUMENT_VERSION",
-                class: "action-green",
-                showInView: true,
-                checkShow: self.checkToShowAction
-            },
-            // duplicate current version
-            {
-                type: 'action',
-                icon: 'content-copy',
-                text: 'grid_action_duplication_current_version',
-                shortcut: false,
-                callback: self.duplicateCurrentVersion,
-                class: "action-green",
-                permissionKey: 'DUPLICATE_BOOK_CURRENT',
-                showInView: true,
-                checkShow: function (action, model) {
-                    var info = model.getInfo();
-                    return self.checkToShowAction(action, model) && (info.documentClass === 'outgoing' || info.documentClass === 'internal') && !info.isPaper;
-                }
-            },
-            // duplicate specific version
-            {
-                type: 'action',
-                icon: 'content-duplicate',
-                text: 'grid_action_duplication_specific_version',
-                shortcut: false,
-                callback: self.duplicateVersion,
-                class: "action-green",
-                showInView: true,
-                permissionKey: 'DUPLICATE_BOOK_FROM_VERSION',
-                checkShow: self.checkToShowAction
+                showInView: false,
+                checkShow: self.checkToShowAction,
+                permissionKey: [
+                    "DUPLICATE_BOOK_CURRENT",
+                    "DUPLICATE_BOOK_FROM_VERSION"
+                ],
+                checkAnyPermission: true,
+                subMenu: [
+                    // duplicate current version
+                    {
+                        type: 'action',
+                        icon: 'content-copy',
+                        text: 'grid_action_duplication_current_version',
+                        shortcut: false,
+                        callback: self.duplicateCurrentVersion,
+                        class: "action-green",
+                        permissionKey: 'DUPLICATE_BOOK_CURRENT',
+                        showInView: true,
+                        checkShow: function (action, model) {
+                            var info = model.getInfo();
+                            return self.checkToShowAction(action, model) && (info.documentClass === 'outgoing' || info.documentClass === 'internal') && !info.isPaper;
+                        }
+                    },
+                    // duplicate specific version
+                    {
+                        type: 'action',
+                        icon: 'content-duplicate',
+                        text: 'grid_action_duplication_specific_version',
+                        shortcut: false,
+                        callback: self.duplicateVersion,
+                        class: "action-green",
+                        showInView: true,
+                        permissionKey: 'DUPLICATE_BOOK_FROM_VERSION',
+                        checkShow: self.checkToShowAction
+                    }
+                ]
             }
         ];
     });
