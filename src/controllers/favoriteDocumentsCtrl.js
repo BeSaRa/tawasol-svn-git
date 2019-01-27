dde11e0eed6bfdc604833bcac3681624a43b33e2
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-01-27 13:58:17 +0000

    add view in desktop action to (sentItems, favorite,folder,followup, proxyMail) svn: E155011: Commit failed (details follow)
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@3310 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/controllers/favoriteDocumentsCtrl.js
+++ src/controllers/favoriteDocumentsCtrl.js
@@ -416,6 +416,10 @@ module.exports = function (app) {
                 });
         };
 
+        self.viewInDeskTop = function (workItem) {
+            return correspondenceService.viewWordInDesktop(workItem);
+        };
+
         /**
          * @description Array of actions that can be performed on grid
          * @type {[*]}
@@ -496,6 +500,22 @@ module.exports = function (app) {
                         class: "action-green",
                         showInView: true,
                         checkShow: self.checkToShowAction
+                    },
+                    // viewInDeskTop
+                    {
+                        type: 'action',
+                        icon: 'monitor',
+                        text: 'grid_action_view_in_desktop',
+                        shortcut: false,
+                        hide: false,
+                        callback: self.viewInDeskTop,
+                        class: "action-green",
+                        permissionKey: 'VIEW_DOCUMENT',
+                        showInView: false,
+                        checkShow: function (action, model) {
+                            var info = model.getInfo();
+                            return self.checkToShowAction(action, model) && info.needToApprove();
+                        }
                     }
                 ]},
             // Separator
--- src/controllers/followupEmployeeInboxCtrl.js
+++ src/controllers/followupEmployeeInboxCtrl.js
@@ -638,6 +638,10 @@ module.exports = function (app) {
                 });
         };
 
+        self.viewInDeskTop = function (workItem) {
+            return correspondenceService.viewWordInDesktop(workItem);
+        };
+
         /**
          * @description Array of actions that can be performed on grid
          * @type {[*]}
@@ -719,6 +723,22 @@ module.exports = function (app) {
                         showInView: true,
                         showInViewOnly: true,
                         checkShow: self.checkToShowAction
+                    },
+                    // viewInDeskTop
+                    {
+                        type: 'action',
+                        icon: 'monitor',
+                        text: 'grid_action_view_in_desktop',
+                        shortcut: false,
+                        hide: false,
+                        callback: self.viewInDeskTop,
+                        class: "action-green",
+                        permissionKey: 'VIEW_DOCUMENT',
+                        showInView: false,
+                        checkShow: function (action, model) {
+                            var info = model.getInfo();
+                            return self.checkToShowAction(action, model) && info.needToApprove();
+                        }
                     }
                 ]
             },
