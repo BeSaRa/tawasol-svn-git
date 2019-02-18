327b582ba961f214851ee8f8cbb58ed876e64f93
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-02-18 07:18:15 +0000

    fix: mailNotificationService changes reverted
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@3445 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/services/mailNotificationService.js
+++ src/services/mailNotificationService.js
@@ -13,18 +13,16 @@ module.exports = function (app) {
         self.notifications = [];
         self.unreadNotificationsCount = 0;
         self.notificationsRequestCount = 10;
-        self.loadMailNotifications = function (count, onlySubscriptions) {
+        self.loadMailNotifications = function (count) {
             count = count ? count : self.notificationsRequestCount;
             return $http
                 .get(urlService.notifications.change({count: count}), {
                     excludeLoading: true
                 })
                 .then(function (result) {
-                    if (onlySubscriptions) {
-                        var notifications = result.data.rs.first;
-                        self.unreadNotificationsCount = _.filter(notifications, {'isOpen': false}).length;
-                        self.notifications = generator.interceptReceivedCollection('MailNotification', generator.generateCollection(notifications, MailNotification));
-                    }
+                    var notifications = result.data.rs.first;
+                    self.unreadNotificationsCount = _.filter(notifications, {'isOpen': false}).length;
+                    self.notifications = generator.interceptReceivedCollection('MailNotification', generator.generateCollection(notifications, MailNotification));
                     userSubscriptionService.userSubscriptions = result.data.rs.second;
 
                     return self.notifications;
