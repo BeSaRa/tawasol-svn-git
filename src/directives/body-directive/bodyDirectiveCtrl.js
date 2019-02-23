b841216a5d4807e8751277749141f15962514880
besara <besara@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-02-23 14:10:50 +0000

    BUNDLE FOR TEST [#3491]
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@3491 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/directives/body-directive/bodyDirectiveCtrl.js
+++ src/directives/body-directive/bodyDirectiveCtrl.js
@@ -83,7 +83,7 @@ module.exports = function (app) {
         });
 
         $scope.bundleVersion = '2.1.2';
-        $scope.buildNumber = ' T#3483';
-        app.$_privateBuildNumber = 'T#3483';
+        $scope.buildNumber = ' T#3491';
+        app.$_privateBuildNumber = 'T#3491';
     });
 };
