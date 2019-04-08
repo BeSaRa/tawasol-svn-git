c0146b9c8557fa8f8f5f398eb4415400bedcb45b
besara <besara@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-04-08 07:26:02 +0000

    add fix URL
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@3699 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- dist/js/configuration.js
+++ dist/js/configuration.js
@@ -4,8 +4,8 @@
         .config(function (urlServiceProvider) {
             'ngInject';
             urlServiceProvider
-                .setEnvironment('stage')
-                .setBaseUrl('fix', 'http://100.100.3.220:9080/CMSService/service')
+                .setEnvironment('fix')
+                .setBaseUrl('fix', 'http://100.100.3.220:9080/CMSServicesFix/service')
                 .setBaseUrl('trunk', 'http://100.100.3.220:9080/CMSServices/service')
                 .setBaseUrl('stage', 'http://MDPS-FNWEB01/CMSServices/service')
                 .setBaseUrl('test', 'http://eblaepm.no-ip.org:9081/CMSServices/service')
