46f59c4bfceaffc1ae65faea375c022dea7a679b
besara <besara@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-04-22 00:00:01 +0000

    fix: V2.1.3 - remove MAIP security should be activated only if valid URL is added to root entity URL field
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@3808 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/services/customValidationService.js
+++ src/services/customValidationService.js
@@ -15,7 +15,7 @@ module.exports = function (app) {
             /**
              * @description Allows url format with http or https or www
              */
-            url: /(?:https?:\/\/|www\.)[\d.A-Za-z-]+\.[A-Za-z]{2,6}/,
+            url: /^(http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(\/.*)?|^((http:\/\/www\.|https:\/\/www\.|http:\/\/|https:\/\/)?([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])\.){3}([0-9]|[1-9][0-9]|1[0-9]{2}|2[0-4][0-9]|25[0-5])$/,
             /**
              * @description Allows IP format only
              */
