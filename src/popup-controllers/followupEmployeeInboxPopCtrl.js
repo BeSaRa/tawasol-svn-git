82ed99480473bb7963724372f24c653519345f9e
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-10-14 06:54:50 +0000

    add search for application user
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@4638 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/popup-controllers/followupEmployeeInboxPopCtrl.js
+++ src/popup-controllers/followupEmployeeInboxPopCtrl.js
@@ -18,6 +18,7 @@ module.exports = function (app) {
         self.controllerName = 'followupEmployeeInboxPopCtrl';
 
         self.inlineOUSearchText = '';
+        self.inlineAppUserSearchText = '';
 
         var _mapRegOUSections = function(){
             // filter all regOU
