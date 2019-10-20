56fea5412e31b922b7b6e5d36d80b86871c23e28
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-10-20 15:28:26 +0000

    2.2.1: grid indicators legend
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@4672 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/directives/legend-indicators-directive/legend-indicators-directive.js
+++ src/directives/legend-indicators-directive/legend-indicators-directive.js
@@ -1,13 +1,14 @@
 module.exports = function (app) {
+    require('./legend-indicators-style.scss');
     app.directive('legendIndicatorsDirective', function (cmsTemplate) {
         'ngInject';
         return {
             restrict: 'E',
+            templateUrl: cmsTemplate.getDirective('legend-indicators-template.html'),
             controller: 'legendIndicatorsDirectiveCtrl',
             controllerAs: 'ctrl',
             bindToController: true,
             replace: true,
-            templateUrl: cmsTemplate.getDirective('legend-indicators-template.html'),
             scope: {
                 gridLegends: '=',
                 visibleCount: '@?'
