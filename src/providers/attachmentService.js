dc598942cae2c6b3efde481d245ea149536e2599
besara <besara@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-11-19 10:52:47 +0000

    Merged from cms-2.2.0 Update Routes and remove URLEncode for ICN [from revision 4891]
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@4892 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/providers/attachmentService.js
+++ src/providers/attachmentService.js
@@ -103,6 +103,10 @@ module.exports = function (app) {
              * @param rootEntity
              * @param correspondenceService
              * @param fileTypeService
+             * @param tokenService
+             * @param langService
+             * @param tokenService
+             * @param langService
              * @return {provider}
              */
             provider.$get = function (urlService,
