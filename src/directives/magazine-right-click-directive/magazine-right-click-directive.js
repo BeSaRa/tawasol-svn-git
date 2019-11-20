2dfce4014d2afaccc8e62299951a13291a4eb42b
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2019-11-20 08:38:23 +0000

    2.2.1: UX Changes - highlight the row when context menu is opened
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@4903 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/directives/magazine-right-click-directive/magazine-right-click-directive.js
+++ src/directives/magazine-right-click-directive/magazine-right-click-directive.js
@@ -1,7 +1,10 @@
 module.exports = function (app) {
-    app.directive('magazineRightClickDirective', function ($compile, $timeout) {
+    app.directive('magazineRightClickDirective', function ($compile, $timeout, gridService) {
         'ngInject';
         return function (scope, element, attrs) {
+            var $element,// the clicked element.
+                parentRow, contextRowClass = '';
+
             element.bind('contextmenu', function (event) {
                 event.preventDefault();
                 // if there is no model provide to the directive throw an exception to log.
@@ -15,13 +18,20 @@ module.exports = function (app) {
                     return;
                 }
 
+                $element = angular.element(event.target);
+                parentRow = $element.parents('div.magazine-item')[0];
+
+                // contextRowClass is the unique number
+                // It is set as class of parent row and passed to gridActionsDirective.
+                // When menu opens, we will find the row by class=contextRowUniqueClass and highlight it.
+                // When menu with main-context-menu class is closed, the (contextRowUniqueClass, background-context) classes will be removed.
+                contextRowClass = (new Date().valueOf()).toString();
+
 
                 // common variables
                 var
                     // parent wrapper.
                     $parentWrapper = element.parents('.magazine-layout'),
-                    // the clicked element.
-                    $element = angular.element(event.target),
                     // event position from the screen.
                     screen = {
                         left: event.clientX,
@@ -39,7 +49,8 @@ module.exports = function (app) {
                     menu = angular.element('<grid-actions-directive />', {
                         'context-actions': 'ctrl.contextMenuActions',
                         'model': attrs.model,
-                        'actions-direction': 'context'
+                        'actions-direction': gridService.gridActionOptions.direction.context,
+                        'context-row-class': contextRowClass
                     });
 
                 // set the css for the wrapper.
@@ -52,6 +63,7 @@ module.exports = function (app) {
                 element.append(wrapper);
                 // wait ms to trigger click event for the button to open the menu.
                 $timeout(function () {
+                    parentRow.classList.add(contextRowClass);
                     // prevent bubbling.
                     angular
                         .element('.menu-handler')
@@ -60,9 +72,23 @@ module.exports = function (app) {
                         });
                     // trigger a click button to open the menu.
                     wrapper.find('.menu-handler').click();
+                    _handleCloseContextMenu();
                 });
 
             });
+
+            /**
+             * @description Removes the classes(contextRowClass and background-context) when context menu with class(main-context-menu) closes
+             * @private
+             */
+            var _handleCloseContextMenu = function () {
+                // remove the classes(contextRowClass and background-context) when context menu with class(main-context-menu) closes
+                scope.$on("$mdMenuClose", function (event, el) {
+                    if (el[0].classList.contains('main-context-menu')) {
+                        parentRow.classList.remove('background-context', contextRowClass);
+                    }
+                });
+            };
         }
     })
 };
