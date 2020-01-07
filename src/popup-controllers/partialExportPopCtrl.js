40075680e10bca076d4dc703e0896182c88f4b43
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2020-01-07 11:25:12 +0000

    Merged from cms-2.2.1 Merged from cms-2.2.0 partial export searchable drop down [from revision 5260] [from revision 5265]
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@5266 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/popup-controllers/partialExportPopCtrl.js
+++ src/popup-controllers/partialExportPopCtrl.js
@@ -55,6 +55,7 @@ module.exports = function (app) {
         self.selectedSiteType = null;
 
         self.selectedMainSite = null;
+        self.selectedMainSiteSearchText = '';
 
         self.subRecords = [];
         _concatCorrespondenceSites(false);
@@ -584,6 +585,26 @@ module.exports = function (app) {
                 .catch(function (selectedCorrespondences) {
                     self.partialExportList.setAttachmentLinkedDocs(selectedCorrespondences);
                 })
+        };
+
+        /**
+         * @description Clears the searchText for the given field
+         * @param fieldType
+         */
+        self.clearSearchText = function (fieldType) {
+            self[fieldType + 'SearchText'] = '';
+        };
+
+        /**
+         * @description Prevent the default dropdown behavior of keys inside the search box of workflow action dropdown
+         * @param $event
+         */
+        self.preventSearchKeyDown = function ($event) {
+            if ($event) {
+                var code = $event.which || $event.keyCode;
+                if (code !== 38 && code !== 40)
+                    $event.stopPropagation();
+            }
         };
 
         /**
