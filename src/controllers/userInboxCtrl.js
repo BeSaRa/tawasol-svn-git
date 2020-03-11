6c11d86ebcc33dabd383d2543cb69dcc63fa20f8
besara <besara@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2020-03-11 13:36:42 +0000

    send main document by email
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@5680 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/controllers/userInboxCtrl.js
+++ src/controllers/userInboxCtrl.js
@@ -1912,7 +1912,7 @@ module.exports = function (app) {
                         type: 'action',
                         icon: 'link-variant',
                         text: 'grid_action_link_to_document_by_email',
-                        permissionKey: 'SEND_LINK_TO_THE_DOCUMENT_BY_EMAIL',
+                        permissionKey: 'SEND_COMPOSITE_DOCUMENT_BY_EMAIL',
                         callback: self.sendLinkToDocumentByEmail,
                         class: "action-green",
                         checkShow: function (action, model) {
