2b7e04646e81f407aef65564b1e5d04ca82a56aa
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2020-04-14 10:24:28 +0000

    fix: timeout for calculation in table-height-directive
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@5842 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/directives/table-height-directive/table-height-directive.js
+++ src/directives/table-height-directive/table-height-directive.js
@@ -1,15 +1,18 @@
 module.exports = function (app) {
-    app.directive('tableHeightDirective', function ($window) {
+    app.directive('tableHeightDirective', function ($window, $timeout) {
         'ngInject';
         return {
             restrict: 'A',
             link: function (scope, element) {
-                var position = angular.element(element).offset();
 
-                var mHeight = position.top + 70;
-                _calculate();
+                $timeout(function () {
+                    _calculate();
+                },1000);
 
                 function _calculate() {
+                    var position = angular.element(element).offset();
+
+                    var mHeight = position.top + 70;
                     element.css({
                         overflow: 'auto',
                         maxHeight: (window.innerHeight - mHeight)
