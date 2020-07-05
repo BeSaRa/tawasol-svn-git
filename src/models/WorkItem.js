12dabaeff5d2fd8c34b8b171b453076711e95b43
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2020-07-05 13:55:21 +0000

    Feature: Conditional Approve popup
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@6214 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/models/WorkItem.js
+++ src/models/WorkItem.js
@@ -672,10 +672,87 @@ module.exports = function (app) {
                 return dialog
                     .showDialog({
                         templateUrl: cmsTemplate.getPopup('conditional-approve'),
-                        controller: function () {
+                        controller: function (dialog, correspondence, comments, $scope, generator, $timeout) {
                             'ngInject';
+                            var self = this;
+
+                            self.correspondence = correspondence;
+                            self.comments = comments;
+                            self.conditionalApproveForm = null;
+
+                            self.model = {
+                                exportDate: generator.getFutureDate(1),
+                                comments: null
+                            };
+
+                            self.minDate = generator.getFutureDate(1);
+                            self.minDate.setHours(0, 0, 0, 0);
+                            self.minDateString = moment(self.minDate).format(generator.defaultDateFormat);
+
+                            self.selectedComment = null;
+                            self.commentSearchText = '';
+
+                            self.setComment = function () {
+                                self.model.comments = self.selectedComment.getComment();
+                            };
+
+                            /**
+                             * @description Checks if the data is valid
+                             * @param form
+                             * @returns {boolean}
+                             */
+                            self.isValidModel = function (form) {
+                                form = form || self.conditionalApproveForm;
+                                return form.$valid
+                                    && (!!self.model.exportDate && generator.getTimeStampFromDate(self.model.exportDate) >= generator.getTimeStampFromDate(self.minDate))
+                                    && (!!self.model.comments);
+                            };
+
+                            /**
+                             * @description Approves the correspondence
+                             * @param $event
+                             */
+                            self.approve = function ($event) {
+                                if (!self.isValidModel()) {
+                                    return;
+                                }
+                                dialog.hide(self.model);
+                            };
+
+                            self.closePopup = function ($event) {
+                                dialog.cancel('CONDITIONAL_APPROVE_REJECTED');
+                            };
+
+                            /**
+                             * @description Clears the searchText for the given field
+                             * @param fieldType
+                             */
+                            self.clearSearchText = function (fieldType) {
+                                self[fieldType + 'SearchText'] = '';
+                            };
+
+                            /**
+                             * @description Prevent the default dropdown behavior of keys inside the search box of dropdown
+                             * @param $event
+                             */
+                            self.preventSearchKeyDown = function ($event) {
+                                if ($event) {
+                                    var code = $event.which || $event.keyCode;
+                                    if (code !== 38 && code !== 40)
+                                        $event.stopPropagation();
+                                }
+                            };
+
+                            self.$onInit = function () {
+                                $timeout(function () {
+                                    self.conditionalApproveForm = $scope.conditionalApproveForm;
+                                });
+                            }
                         },
                         controllerAs: 'ctrl',
+                        locals: {
+                            correspondence: this
+                        },
                         resolve: {
                             comments: function (userCommentService) {
                                 'ngInject';
@@ -683,8 +760,8 @@ module.exports = function (app) {
                             }
                         }
                     })
-                    .then(function () {
-
+                    .then(function (data) {
+                        console.log('data', data);
                     });
             };
 
