2defc20532a818dd1acdce165c90a73f2e478d83
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2020-09-14 11:00:33 +0000

    fix: Scroll bar for menu, context menu
    fix: scroll bar design for firefox(similar to perfect scrollbar)
    
    fix 33406:V_2.4.0: New added data sources (group inbox , incoming mail)at dashboard not appear in list from normal window size(add scroll down in menu)
    
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@6633 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/directives/grid-context-menu-directive/grid-context-menu-directive.js
+++ src/directives/grid-context-menu-directive/grid-context-menu-directive.js
@@ -7,5 +7,19 @@ module.exports = function (app) {
             controller: 'gridContextMenuDirectiveCtrl',
             controllerAs: 'contextCtrl'
         }
+    }).directive('mdMenuDefaultScroll', function ($window) {
+        'ngInject';
+        return {
+            restrict: 'AC',
+            link: function (scope, element, attrs) {
+                var menuListener = scope.$on('$mdMenuOpen', () => {
+                    $window.requestAnimationFrame(() => {
+                        // Using parent() method to get parent wrapper with .md-open-menu-container class and adding custom class.
+                        element.parent().addClass('menu-default-scroll');
+                        menuListener();
+                    });
+                });
+            }
+        }
     })
 };
