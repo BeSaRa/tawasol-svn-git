2f7afd432904c4c5a71620c489336485782c0634
besara <besara@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2021-03-14 08:59:13 +0000

    BUNDLE FOR TEST [T#7222]
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@7222 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -17,8 +17,8 @@ module.exports = function (app) {
 
         versionServiceProvider
             .setVersionNumber('2.4.0')
-            .setBuildNumber('T#7217')
-            .setPrivateBuildNumber('T#7217');
+            .setBuildNumber('T#7222')
+            .setPrivateBuildNumber('T#7222');
 
 
         $urlRouterProvider.otherwise('/404');
