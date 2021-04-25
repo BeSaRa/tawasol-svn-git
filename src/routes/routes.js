89a21087175b0bdc1701a0bf7076f3bb51379393
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2021-04-25 06:54:30 +0000

    fix removed line, route not redirecting
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@7311 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -19,6 +19,9 @@ module.exports = function (app) {
             .setVersionNumber('2.4.0')
             .setBuildNumber('T#7292')
             .setPrivateBuildNumber('T#7292');
+
+        $urlRouterProvider.otherwise('/404');
+
         /**
          * md-datepicker settings for default date format
          * @param date
