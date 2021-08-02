cd5b09c6b5ba75ee67feb9f1730602779d51824b
besara <besara@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2021-08-02 12:35:40 +0000

    Fix: XSS Scripting reported from MOF.
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@7512 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/decorators/01-index.js
+++ src/decorators/01-index.js
@@ -3,4 +3,5 @@ module.exports = function (app) {
     require('./$pagination')(app);
     require('./mdTable')(app);
     require('./$textAngular')(app);
+    require('./tooltipDecorator')(app);
 };
