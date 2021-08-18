575cfa86ce652a1ac17e926674a0616cae359d62
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2021-08-18 13:13:28 +0000

    feature 2.4.1: flip login
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@7598 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/models/GlobalSetting.js
+++ src/models/GlobalSetting.js
@@ -68,6 +68,7 @@ module.exports = function (app) {
             self.externalAuthorization = false;
             self.limitedCentralUnitAccess = false;
             self.outofofficeFromAllUsers = false;
+            self.flipLogin = false;
             //endregion
 
             // every model has required fields
