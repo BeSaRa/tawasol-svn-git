036471941cbaee77d241c08c9caf789a6a12d4ca
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2021-08-18 13:30:51 +0000

    fix 43104 V 2.4.1 regression : Error happens if we try to change property configuration
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@7599 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/factories/errorCode.js
+++ src/factories/errorCode.js
@@ -82,7 +82,8 @@ module.exports = function (app) {
                 G2G_CANT_RETURN_AS_ALREADY_RECEIVED: 14014,
                 ATTACHMENT_RESTRICTED_TO_MODIFY_AFTER_BOOK_AUTHORIZATION: 3035,
                 NO_DATA_IN_DATASOURCE: 1011,
-                SEARCH_OPERATION_FAILED: 2004
+                SEARCH_OPERATION_FAILED: 2004,
+                BL_ERROR_UPDATE_GLOBAL_PROPERTY_CONFIG: 3079
             },
             errorLangKeys = {
                 1001: 'no_records_found',
@@ -121,7 +122,8 @@ module.exports = function (app) {
                 3051: 'cannot_export_more_than_fifty_attachments_or_linked_documents_to_g2g',
                 14014: 'g2g_cant_return_as_already_received',
                 2004: 'search_operation_failed',
-                3035: 'attachment_restricted_to_modify_after_book_authorization'
+                3035: 'attachment_restricted_to_modify_after_book_authorization',
+                3079: 'bl_error_update_global_property_config'
             };
 
         return {
