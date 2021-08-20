f9d70aadb3530a13d5572a05b7bfcb4ac6fc3d82
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2021-08-20 13:59:49 +0000

    fix: add document > default dropdown selection after reset
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@7607 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/directives/manage-correspondence-site-incoming-directive/manageCorrespondenceSiteIncomingDirectiveCtrl.js
+++ src/directives/manage-correspondence-site-incoming-directive/manageCorrespondenceSiteIncomingDirectiveCtrl.js
@@ -99,37 +99,39 @@ module.exports = function (app) {
         }
 
         var _resetDefaultNeedReplyFollowupDate = function () {
-            if (self.correspondence) {
-                var priorityLevel = self.correspondence.getInfo().priorityLevel;
-                priorityLevel = priorityLevel.hasOwnProperty('lookupKey') ? priorityLevel.lookupKey : priorityLevel;
+            $timeout(function (){
+                if (self.correspondence) {
+                    var priorityLevel = self.correspondence.getInfo().priorityLevel;
+                    priorityLevel = priorityLevel.hasOwnProperty('lookupKey') ? priorityLevel.lookupKey : priorityLevel;
 
-                var slaOu = null;
-                // if paper outgoing or incoming and current ou is central archive, use selected registryOu as slaOu
-                if (_isShowRegistryUnit() && organizationForSLACentralArchive) {
-                    slaOu = organizationForSLACentralArchive;
-                } else {
-                    slaOu = organizationForSLA;
-                }
-                if (typeof priorityLevel !== 'undefined' && priorityLevel !== null) {
-                    var slaDays = null;
-                    // organization has sla property and sla has property as same as document priority level
-                    if (slaOu.hasOwnProperty('sla') && slaOu.sla && slaOu.sla.hasOwnProperty(priorityLevel)) {
-                        slaDays = slaOu.sla[priorityLevel];
+                    var slaOu = null;
+                    // if paper outgoing or incoming and current ou is central archive, use selected registryOu as slaOu
+                    if (_isShowRegistryUnit() && organizationForSLACentralArchive) {
+                        slaOu = organizationForSLACentralArchive;
+                    } else {
+                        slaOu = organizationForSLA;
                     }
-                    // if no SLA days or its less than 1, use default number of days to followup date to be today
-                    if (!slaDays || slaDays < 1) {
-                        slaDays = angular.copy(defaultFollowupNumberOfDays);
+                    if (typeof priorityLevel !== 'undefined' && priorityLevel !== null) {
+                        var slaDays = null;
+                        // organization has sla property and sla has property as same as document priority level
+                        if (slaOu.hasOwnProperty('sla') && slaOu.sla && slaOu.sla.hasOwnProperty(priorityLevel)) {
+                            slaDays = slaOu.sla[priorityLevel];
+                        }
+                        // if no SLA days or its less than 1, use default number of days to followup date to be today
+                        if (!slaDays || slaDays < 1) {
+                            slaDays = angular.copy(defaultFollowupNumberOfDays);
+                        }
+                        defaultNeedReplyFollowupDate = generator.getFutureDate(slaDays);
                     }
-                    defaultNeedReplyFollowupDate = generator.getFutureDate(slaDays);
                 }
-            }
 
 
-            // set followup date for all searched sites
-            if (self.subSearchResultCopy && self.subSearchResultCopy.length) {
-                self.onFollowupStatusChange(self.followupStatus);
-            }
-            return defaultNeedReplyFollowupDate;
+                // set followup date for all searched sites
+                if (self.subSearchResultCopy && self.subSearchResultCopy.length) {
+                    self.onFollowupStatusChange(self.followupStatus);
+                }
+                return defaultNeedReplyFollowupDate;
+            })
         };
 
         /**
