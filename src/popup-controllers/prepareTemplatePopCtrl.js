d486ae6528d0b4a973d9deb9f946fdd78bc16f83
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2021-09-01 10:54:41 +0000

    kwt: add search to templates popup
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@7634 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/popup-controllers/prepareTemplatePopCtrl.js
+++ src/popup-controllers/prepareTemplatePopCtrl.js
@@ -3,6 +3,7 @@ module.exports = function (app) {
         'ngInject';
         var self = this;
         self.controllerName = 'prepareTemplatePopCtrl';
+        self.templateSearchText = null;
 
         self.sendTemplateToPrepare = function (template) {
             dialog.hide(template);
