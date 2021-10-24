6ba7a4071a1782fa2c0768b0dc79ad19973f5ba4
navjot <navjot@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2021-10-24 09:12:55 +0000

    BUNDLE FOR TEST[TK#7751]
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@7751 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -17,8 +17,8 @@ module.exports = function (app) {
 
         versionServiceProvider
             .setVersionNumber('2.4.1')
-            .setBuildNumber('TK#7744')
-            .setPrivateBuildNumber('TK#7744');
+            .setBuildNumber('TK#7751')
+            .setPrivateBuildNumber('TK#7751');
 
         $urlRouterProvider.otherwise('/404');
 
