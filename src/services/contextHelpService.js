769d3c3cbfd5e1dc702582c90912f5d3f45918bd
khalid <khalid@3f25d8b1-e65f-8d48-bd54-75b34d8be7f1>
2021-11-14 11:47:20 +0000

    kwt: add videos help
    git-svn-id: https://win-lhoa5a3q3i7/svn/front-end-backup/trunk@7783 3f25d8b1-e65f-8d48-bd54-75b34d8be7f1

--- src/services/contextHelpService.js
+++ src/services/contextHelpService.js
@@ -8,6 +8,7 @@ module.exports = function (app) {
         self.defaultHelpUrl = 'help/';
 
         self.currentHelpUrl = null;
+        self.currentHelpID = null;
 
         self.excludedControllers = [
             'documentsNotifyDirectiveCtrl'
@@ -17,6 +18,7 @@ module.exports = function (app) {
             if (excludeMe) {
                 return;
             }
+            self.currentHelpID = helpID;
             self.currentHelpUrl = self.defaultHelpUrl + helpID;
         };
         /**
@@ -26,7 +28,19 @@ module.exports = function (app) {
             return dialog
                 .showDialog({
                     templateUrl: self.currentHelpUrl + '_' + langService.current + '_help.html',
-                    targetEvent: $event || false
+                    targetEvent: $event || false,
+                    controller: function ($compile, $element, $timeout, configurationService) {
+                        if (!configurationService.G2G_QATAR_SOURCE) {
+                            var link = document.createElement('a');
+                            link.innerText = langService.get('explanation_video');
+                            link.href = 'help_videos/' + self.currentHelpID + '.mp4';
+                            link.target = "_blank";
+                            $timeout(function () {
+                                angular.element($element[0].querySelector('md-content')).prepend(link);
+                            })
+                        }
+                    },
+                    controllerAs: "ctrl"
                 })
                 .then(function () {
 
