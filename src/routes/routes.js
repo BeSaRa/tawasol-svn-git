8869086adc8e7f38246dbce2536b32906b04ebca
Khaled Yabroudi <khaledyabroudi@gmail.com>
2022-02-20 09:47:51 +0300

    BUNDLE FOR TEST[TG#35]

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -17,8 +17,8 @@ module.exports = function (app) {
 
         versionServiceProvider
             .setVersionNumber('2.5.0')
-            .setBuildNumber('TG#34')
-            .setPrivateBuildNumber('TG#34');
+            .setBuildNumber('TG#35')
+            .setPrivateBuildNumber('TG#35');
 
         $urlRouterProvider.otherwise('/404');
 
