a739c00017288ec7fb62a03a965f97a90fd97ae1
Khaled Yabroudi <khaledyabroudi@gmail.com>
2022-06-26 10:28:27 +0300

    QAF: make searchable table instead of dropdown in ou classifications

--- src/popup-controllers/classificationSelectorPopCtrl.js
+++ src/popup-controllers/classificationSelectorPopCtrl.js
@@ -1,15 +1,17 @@
 module.exports = function (app) {
-    app.controller('classificationSelectorPopCtrl', function (_, langService, dialog, classificationService, excluded) {
+    app.controller('classificationSelectorPopCtrl', function (_, langService,
+                                                              dialog,
+                                                              classificationService,
+                                                              excluded,
+                                                              $filter,
+                                                              gridService) {
         'ngInject';
         var self = this;
         self.controllerName = 'classificationSelectorPopCtrl';
 
         self.selectedClassifications = [];
-
-        self.classification = null;
-
-        self.selectedClassificationGrid = [];
-
+        self.selectedClassificationsAdded = [];
+        self.selectedClassificationsGrid = [];
         self.classifications = classificationService.classifications;
 
         self.grid = {
@@ -19,10 +21,31 @@ module.exports = function (app) {
             limitOptions: [10, 15, 20, {
                 label: langService.get('all'),
                 value: function () {
-                    return (self.ouCorrespondenceSites.length + 21);
+                    return (self.classifications.length + 21);
+                }
+            }],
+            searchColumns: {
+                arabicName: 'arName',
+                englishName: 'enName'
+            },
+            searchText: '',
+            searchCallback: function (grid) {
+                self.classifications = gridService.searchGridData(self.grid, self.classificationsCopy);
+            }
+        };
+
+        self.gridAdded = {
+            limit: 10, // default limit
+            page: 1, // first page
+            order: 'arName', // default sorting order
+            limitOptions: [10, 15, 20, {
+                label: langService.get('all'),
+                value: function () {
+                    return (self.selectedClassifications.length + 21);
                 }
             }]
         };
+
 
         // the excluded must be Classification Instance.
         self.excluded = _.map(excluded, 'id');
@@ -32,7 +55,7 @@ module.exports = function (app) {
             return self.excluded.indexOf(classification.id) === -1;
         };
 
-        self.includeIfEnabled = function(classification){
+        self.includeIfEnabled = function (classification) {
             return classification.status;
         };
 
@@ -41,12 +64,20 @@ module.exports = function (app) {
         };
 
         self.addClassificationToSelectedClassification = function () {
-            self.selectedClassifications = self.selectedClassifications.concat(self.classification);
-            self.excluded = self.excluded.concat(_.map(self.classification, 'id'));
-            self.classification = null;
-            self.selectedClassificationGrid = [];
+            self.selectedClassifications = self.selectedClassifications.concat(self.selectedClassificationsGrid);
+            self.excluded = self.excluded.concat(_.map(self.selectedClassificationsGrid, 'id'));
+            self.classifications = self.filterClassifications(self.classifications);
+            self.classificationsCopy = angular.copy(self.classifications);
+            self.selectedClassificationsGrid = [];
+            self.selectedClassificationsAdded = [];
         };
 
+        /**
+         * @description Gets the grid records by sorting
+         */
+        self.getSortedData = function () {
+            self.classifications = $filter('orderBy')(self.classifications, self.grid.order);
+        };
 
         self.removeClassification = function (classification, doNotclear) {
             var classificationId = classification.id;
@@ -57,20 +88,42 @@ module.exports = function (app) {
                 return classification.id !== classificationId;
             });
             if (!doNotclear)
-                self.selectedClassificationGrid = [];
-        };
+                self.selectedClassificationsAdded = [];
 
-        self.isDisabled = function () {
-            return !self.classification;
+            self.classifications = self.filterClassifications([classification]);
+            self.classificationsCopy = angular.copy(self.classifications);
         };
 
         self.removeSelectedClassification = function () {
-            for (var i = 0; i < self.selectedClassificationGrid.length; i++) {
-                self.removeClassification(self.selectedClassificationGrid[i], true);
+            for (var i = 0; i < self.selectedClassificationsAdded.length; i++) {
+                self.removeClassification(self.selectedClassificationsAdded[i], true);
             }
-            self.selectedClassificationGrid = [];
+            self.selectedClassificationsAdded = [];
         };
 
+        /**
+         * @description Gets the grid records by sorting
+         */
+        self.getSortedData = function () {
+            self.classifications = $filter('orderBy')(self.classifications, self.grid.order);
+        };
 
+        /**
+         * @description Gets the grid records by sorting
+         */
+        self.getSortedDataAdded = function () {
+            self.selectedClassifications = $filter('orderBy')(self.selectedClassifications, self.grid.order);
+        };
+
+        self.filterClassifications = function (classifications) {
+            return _.filter(classifications, function (classification) {
+                return self.excludeIfExists(classification) && self.includeIfEnabled(classification) && !classification.isGlobal
+            })
+        }
+
+        self.$onInit = function () {
+            self.classifications = self.filterClassifications(self.classifications);
+            self.classificationsCopy = angular.copy(self.classifications);
+        }
     });
 };
